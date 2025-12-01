6a450c9cd2568209725e514c316577fa2e09875f
oliviercar0n <oliviercaron28@gmail.com>
2025-12-01 00:18:40 -0500

    Add 2015 day 1

---
+++ src/2025/day-01.py
@@ -0,0 +1,59 @@
+from collections import Counter
+from math import floor
+
+with open("day-01.txt", "r") as f:
+    input_data = f.read().strip().split("\n")
+
+# Part 1
+
+pos = 50
+acc = 0
+for ins in input_data:
+    d = ins[0]
+    i = int(ins[1:])
+
+    x = floor(i / 100)
+
+    i -= x * 100
+
+    if d == "L":
+        pos -= i
+    elif d == "R":
+        pos += i
+
+    if pos >= 100:
+        pos -= 100
+    elif pos < 0:
+        pos += 100
+
+    if pos == 0:
+        acc += 1
+
+
+print(acc)
+
+# Part 2
+
+pos = 50
+acc = 0
+for ins in input_data:
+    d = ins[0]
+    i = int(ins[1:])
+
+    if d == "L":
+        inc = -1
+    else:
+        inc = 1
+
+    for _ in range(i):
+        pos += inc
+
+        if pos >= 100:
+            pos -= 100
+        elif pos < 0:
+            pos += 100
+
+        if pos == 0:
+            acc += 1
+
+print(acc)
